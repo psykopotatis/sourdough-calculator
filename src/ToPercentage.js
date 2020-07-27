d4faed86d9ae9b410f1a195f73303f3b2ecd13d4
Ingemar <iingemar@gmail.com>
2020-07-28 00:35:35 +0200

    Added working toPercentage

--- src/ToPercentage.js
+++ src/ToPercentage.js
@@ -1,55 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-class ToPercentage extends Component {
+class ToGrams extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            toGrams: true,
-            toPercentage: false,
+            showOutput: false,
             flour: 1000,
             water: 0,
-            waterPercent: 80,
+            waterPercent: 0,
             sourdough: 0,
-            sourdoughPercent: 15,
+            sourdoughPercent: 0,
             salt: 0,
-            saltPercent: 2
+            saltPercent: 0
 
         };
 
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);        
+        this.handleClick = this.handleClick.bind(this);
         this.onInputChange = this.onInputChange.bind(this);
     }
 
     handleClick(e) {
         e.preventDefault();
-        console.table(this.state);
-        if (this.state.toGrams) {
-            this.calculateGrams();
-        } else {
-            this.calculatePercentages();
-        }
-    };
-
-    calculateGrams() {
-        console.log('calculateGrams');
-
-        const water = this.state.flour * (this.state.waterPercent / 100);
-        const sourdough = this.state.flour * (this.state.sourdoughPercent / 100);
-        const salt = this.state.flour * (this.state.saltPercent / 100);
-
-        this.setState({
-            water: water,
-            sourdough: sourdough,
-            salt: salt,
-        });
+        this.calculatePercentages();
     };
 
     calculatePercentages() {
-        console.log('calculatePercentages');
         const waterPercent = (this.state.water / this.state.flour) * 100;
+        const sourdoughPercent = (this.state.sourdough / this.state.flour) * 100;
+        const saltPercent = (this.state.salt / this.state.flour) * 100;
+
         this.setState({
-            waterPercent: waterPercent
+            showOutput: true,
+            waterPercent: waterPercent,
+            sourdoughPercent: sourdoughPercent,
+            saltPercent: saltPercent,
         });
     };
 
@@ -62,117 +46,155 @@ class ToPercentage extends Component {
 
     render() {
         return (
-            <form>
-
-                <div className="row">
-                    <div className="col-sm">
-                        <div className="form-group">
-                            <label htmlFor="flourInput">Mjöl gram</label>
-                            <input type="text"
-                                   value={this.state.flour}
-                                   onChange={this.onInputChange}
-                                   className="form-control form-control-lg"
-                                   name="flour"
-                                   id="flourInput"
-                            />
-                        </div>
-                    </div>
-                    <div className="col-sm">
-
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-sm">
-                        <div className="form-group">
-                            <label htmlFor="waterInput">Vatten gram</label>
-                            <input type="text"
-                                   value={this.state.water}
-                                   onChange={this.onInputChange}
-                                   className="form-control form-control-lg"
-                                   name="water"
-                                   id="waterInput"
-                                   disabled={this.state.toGrams}
-                            />
-                        </div>
-                    </div>
-                    <div className="col-sm">
-                        <div className="form-group">
-                            <label htmlFor="waterPercentInput">Vatten %</label>
-                            <input type="text"
-                                   value={this.state.waterPercent}
-                                   onChange={this.onInputChange}
-                                   className="form-control form-control-lg"
-                                   name="waterPercent"
-                                   id="waterPercentInput"
-                                   disabled={this.state.toPercentage}
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-sm">
-                        <div className="form-group">
-                            <label htmlFor="sourdoughInput">Surdeg gram</label>
-                            <input type="text"
-                                   value={this.state.sourdough}
-                                   onChange={this.onInputChange}
-                                   className="form-control form-control-lg"
-                                   name="sourdough"
-                                   id="sourdoughInput"
-                                   disabled={this.state.toGrams}
-                            />
-                        </div>
-                    </div>
-                    <div className="col-sm">
-                        <div className="form-group">
-                            <label htmlFor="sourdoughPercentInput">Surdeg %</label>
-                            <input type="text"
-                                   value={this.state.sourdoughPercent}
-                                   onChange={this.onInputChange}
-                                   className="form-control form-control-lg"
-                                   name="sourdoughPercent"
-                                   id="sourdoughPercentInput"
-                                   disabled={this.state.toPercentage}
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-sm">
-                        <div className="form-group">
-                            <label htmlFor="saltInput">Salt gram</label>
-                            <input type="text"
-                                   value={this.state.salt}
-                                   onChange={this.onInputChange}
-                                   className="form-control form-control-lg"
-                                   name="salt"
-                                   id="saltInput"
-                                   disabled={this.state.toGrams}
-                            />
-                        </div>
-                    </div>
-                    <div className="col-sm">
-                        <div className="form-group">
-                                <label htmlFor="saltPercentInput">Salt %</label>
+            <div>
+                <form>
+                    <div className="row">
+                        <div className="col-sm mb-2">
+                            <div className="form-group">
+                                <label htmlFor="flourInput">Mjöl (gram)</label>
                                 <input type="text"
-                                       value={this.state.saltPercent}
+                                       value={this.state.flour}
                                        onChange={this.onInputChange}
                                        className="form-control form-control-lg"
-                                       name="saltPercent"
-                                       id="saltPercentInput"
-                                   disabled={this.state.toPercentage}
+                                       name="flour"
+                                       id="flourInput"
                                 />
+                                <small id="flourHelp" className="form-text text-muted">Man utgår alltid från mjölet! Det
+                                    är 100% och alla andra beräknas utifrån det.</small>
                             </div>
-                    </div>
-                </div>
+                        </div>
+                        <div className="col-sm">
 
-                <button type="button" onClick={this.handleClick} className="btn btn-primary">Räkna!</button>
-            </form>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-sm">
+                            {this.renderWaterInput()}
+                        </div>
+                        <div className="col-sm">
+                            {this.state.showOutput ? this.renderWaterPercentInput() : null}
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-sm">
+                            {this.renderSourdoughInput()}
+                        </div>
+                        <div className="col-sm">
+                            {this.state.showOutput ? this.renderSourdoughPercentInput() : null}
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-sm">
+                            {this.renderSaltInput()}
+                        </div>
+                        <div className="col-sm">
+                            {this.state.showOutput ? this.renderSaltPercentInput() : null}
+                        </div>
+                    </div>
+
+                    <button type="button" onClick={this.handleClick} className="btn btn-primary">Räkna</button>
+                </form>
+            </div>
+        );
+    }
+
+    renderWaterInput() {
+        return (
+            <div className="form-group">
+                <label htmlFor="waterInput">Vatten (gram)</label>
+                <input type="text"
+                       value={this.state.water}
+                       onChange={this.onInputChange}
+                       className="form-control form-control-lg"
+                       name="water"
+                       id="waterInput"
+                />
+            </div>
+        );
+    }
+
+    renderWaterPercentInput() {
+        return (
+            <div className="form-group">
+                <label htmlFor="waterPercentInput">Vatten (%)</label>
+                <input type="text"
+                       value={this.state.waterPercent}
+                       onChange={this.onInputChange}
+                       className="form-control form-control-lg"
+                       name="waterPercent"
+                       id="waterPercentInput"
+                       disabled
+
+                />
+            </div>
+        );
+    }
+
+    renderSourdoughInput() {
+        return (
+            <div className="form-group">
+                <label htmlFor="sourdoughInput">Surdeg (gram)</label>
+                <input type="text"
+                       value={this.state.sourdough}
+                       onChange={this.onInputChange}
+                       className="form-control form-control-lg"
+                       name="sourdough"
+                       id="sourdoughInput"
+                />
+            </div>
+        );
+    }
+
+    renderSourdoughPercentInput() {
+        return (
+            <div className="form-group">
+                <label htmlFor="sourdoughPercentInput">Surdeg (%)</label>
+                <input type="text"
+                       value={this.state.sourdoughPercent}
+                       onChange={this.onInputChange}
+                       className="form-control form-control-lg"
+                       name="sourdoughPercent"
+                       id="sourdoughPercentInput"
+                       disabled
+
+                />
+            </div>
+        );
+    };
+
+    renderSaltInput() {
+        return (
+            <div className="form-group">
+                <label htmlFor="saltInput">Salt (gram)</label>
+                <input type="text"
+                       value={this.state.salt}
+                       onChange={this.onInputChange}
+                       className="form-control form-control-lg"
+                       name="salt"
+                       id="saltInput"
+                />
+            </div>
+        );
+    };
+
+    renderSaltPercentInput() {
+        return (
+            <div className="form-group">
+                <label htmlFor="saltPercentInput">Salt (%)</label>
+                <input type="text"
+                       value={this.state.saltPercent}
+                       onChange={this.onInputChange}
+                       className="form-control form-control-lg"
+                       name="saltPercent"
+                       id="saltPercentInput"
+                       disabled
+                />
+            </div>
         );
     }
 }
 
-export default ToPercentage;
+export default ToGrams;
