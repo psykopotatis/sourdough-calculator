35573030dfa2d09172c83981d3020133affe24d0
Ingemar <iingemar@gmail.com>
2020-08-09 20:56:57 +0200

    Create model Recipe

--- sourdough/models.py
+++ sourdough/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(blank=True, max_length=100)
+    flour = models.FloatField()
+    water = models.FloatField()
+    sourdough = models.FloatField()
+    salt = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
