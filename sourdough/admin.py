7d30c80429ead915fe35d216dc0bb9cb4682b972
Ingemar <iingemar@gmail.com>
2020-08-29 21:07:27 +0200

    Added ingredient model

--- sourdough/admin.py
+++ sourdough/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from sourdough.models import Recipe
+from sourdough.models import Ingredient
 
 admin.site.register(Recipe)
+admin.site.register(Ingredient)
--- sourdough/models.py
+++ sourdough/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 
 
-class Recipe(models.Model):
+class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
     created_date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(blank=True, max_length=100)
-    flour = models.FloatField()
-    water = models.FloatField()
-    sourdough = models.FloatField()
-    salt = models.FloatField()
+    weight = models.FloatField(default=0)
+    percent = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
 
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(blank=True, max_length=100)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.name
